dd898f578e501bd08c7f98b7b97da50a58076453
beefy <nate.schultz@outlook.com>
2017-08-16 18:18:56 -0400

    converted tabs to spaces, moved includes to header file

--- tests/gc_malloc_test.c
+++ tests/gc_malloc_test.c
@@ -7,34 +7,34 @@ void gc_malloc_and_free_test();
 
 typedef struct mallocTest
 {
-	char* c;
-	int x;
+    char* c;
+    int x;
 } mallocTest;
 
 int main(void)
 {
-	gc_malloc_and_free_test();
-	return 0;
+    gc_malloc_and_free_test();
+    return 0;
 }
 
 void gc_malloc_and_free_test()
 {
-	mallocTest *mt1, *mt2, *mt3;
-	mt1 = (mallocTest*)gc_malloc(sizeof(mallocTest));
-	printf("Address of mt1 = %p\n", mt1);
-	mt2 = (mallocTest*)gc_malloc(sizeof(mallocTest));
-	printf("Address of mt2 = %p\n", mt2);
+    mallocTest *mt1, *mt2, *mt3;
+    mt1 = (mallocTest*)gc_malloc(sizeof(mallocTest));
+    printf("Address of mt1 = %p\n", mt1);
+    mt2 = (mallocTest*)gc_malloc(sizeof(mallocTest));
+    printf("Address of mt2 = %p\n", mt2);
 
-	void* ptr = mt1;
-	printf("ptr = %p\n", ptr);
-	gc_free(mt1);
-	
-	mt3 = (mallocTest*)gc_malloc(sizeof(mallocTest));
-	printf("Address of mt3 = %p\n", mt3);
-	assert(mt3 == ptr); // assert that we successfully reused mem location at mt1
+    void* ptr = mt1;
+    printf("ptr = %p\n", ptr);
+    gc_free(mt1);
 
-	gc_free(mt2);
-	gc_free(mt3);
+    mt3 = (mallocTest*)gc_malloc(sizeof(mallocTest));
+    printf("Address of mt3 = %p\n", mt3);
+    assert(mt3 == ptr); // assert that we successfully reused mem location at mt1
 
-	printf("gc_malloc_test: Successful\n");
+    gc_free(mt2);
+    gc_free(mt3);
+
+    printf("gc_malloc_test: Successful\n");
 }
