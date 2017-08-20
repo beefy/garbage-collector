782c61ffadb8ca593d78d9082a97bcc43e3f07fb
beefy <nate.schultz@outlook.com>
2017-08-20 12:32:14 -0400

    changed refs to old structure, fixed warnings

--- gc_malloc/gc_malloc.h
+++ gc_malloc/gc_malloc.h
@@ -15,7 +15,7 @@
 // for memory allocation
 #define MIN_BLOCK_SIZE sizeof(block)
 #define MAX_BLOCKS_TO_ALLOCATE 5
-int blocks_allocated = 0;
+int blocks_allocated;
 typedef struct block
 {
     size_t size;
@@ -37,8 +37,7 @@ void* gc_malloc(size_t size);
 void gc_free(void* p);
 
 // mark and sweep
-void mark();
-void sweep();
-void markAll();
+void mark(block* obj);
+void sweep(block* obj);
 
 #endif
